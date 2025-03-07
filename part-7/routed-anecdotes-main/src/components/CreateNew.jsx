a31f25779db4240d18782ed6a413b1dc50a14bd5
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2025-03-07 05:06:46 +0200

    after all this time LOLL

--- part-7/routed-anecdotes-main/src/components/CreateNew.jsx
+++ part-7/routed-anecdotes-main/src/components/CreateNew.jsx
@@ -1,52 +1,48 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
 
 const CreateNew = ({ addNew }) => {
-    const [content, setContent] = useState("");
-    const [author, setAuthor] = useState("");
-    const [info, setInfo] = useState("");
+    const { form: content, reset: contentReset } = useField("text");
+    const { form: author, reset: authorReset } = useField("text");
+    const { form: info, reset: infoReset } = useField("URL");
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0,
         });
         navigate("/");
     };
 
+    const handelReset = (e) => {
+        e.preventDefault();
+        contentReset();
+        authorReset();
+        infoReset();
+    };
     return (
         <div>
             <h2>create a new anecdote</h2>
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input
-                        name="content"
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                    />
+                    <input {...content} />
                 </div>
                 <div>
                     author
-                    <input
-                        name="author"
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
-                    />
+                    <input {...author} />
                 </div>
                 <div>
                     url for more info
-                    <input
-                        name="info"
-                        value={info}
-                        onChange={(e) => setInfo(e.target.value)}
-                    />
+                    <input {...info} />
                 </div>
                 <button>create</button>
+                <button onClick={handelReset}>Reset</button>
             </form>
         </div>
     );
---
+++ part-7/routed-anecdotes-main/src/hooks/index.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+export const useField = (type) => {
+    const [value, setValue] = useState("");
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+    return {
+        form: { type, value, onChange },
+        reset: () => setValue(""),
+    };
+};
